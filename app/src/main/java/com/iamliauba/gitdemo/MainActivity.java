07e8b461aee4db4f6072352c594c82665defbb4f
getLiauba <36175885+getLiauba@users.noreply.github.com>
2019-09-18 13:28:49 -0400

    Basic working functionality

--- app/src/main/java/com/iamliauba/gitdemo/MainActivity.java
+++ app/src/main/java/com/iamliauba/gitdemo/MainActivity.java
@@ -1,7 +1,11 @@
 package com.iamliauba.gitdemo;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button but = findViewById(R.id.button);
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                CharSequence text = "Hello this is my first Android Application";
+                int duration = Toast.LENGTH_SHORT;
+                Toast.makeText(context,text,duration).show();
+            }
+        });
+
     }
 }
